e1829599ea3baedef1d96f22ab5abf7eef80b98b
Cristian Albuquerque <crslbqrq@gmail.com>
2022-06-20 16:35:50 -0300

    V1.0 Jogo Funcionando, mudei o tema agora irei corrigir o erro da música e do game over.

--- game.py
+++ game.py
@@ -1,17 +1,16 @@
-from turtle import pos
 import pygame
 import random
 pygame.init()
-largura = 800
-altura = 300
+largura = 1024
+altura = 510
 tamanho = (largura, altura)
 pygameDisplay = pygame.display
-pygameDisplay.set_caption("Kuduairo")
+pygameDisplay.set_caption("Dragon Ball Z")
 gameDisplay = pygame.display.set_mode(tamanho)
 gameIcon = pygame.image.load("assets/icone.jpg")
 pygameDisplay.set_icon(gameIcon)
 
-bg = pygame.image.load("assets/fundo.jpg")
+bg = pygame.image.load("assets/fundo3.jpg")
 
 bg_destruido = pygame.image.load("assets/perdeu.jpg")
 
@@ -25,8 +24,8 @@ def dead(pontos):
     pygame.mixer.music.stop()
     fonte = pygame.font.Font("freesansbold.ttf", 40)
     fonteContinue = pygame.font.Font("freesansbold.ttf", 20)
-    texto = fonte.render("Voiçuir perdui com" +str(pontos) + " pontos!", True, white)
-    textoContinue = fonteContinue.rende("Apierte enter paira continuaire...", True, white)
+    texto = fonte.render("Você perdeu com" +str(pontos) + " pontos!", True, white)
+    textoContinue = fonteContinue.render("Aperte enter para continuar...", True, white)
     gameDisplay.blit(textoContinue, (50, 200))
     gameDisplay.blit(texto, (50, 100))
     pygameDisplay.update()
@@ -36,21 +35,18 @@ def jogo():
     posicaoY = random.randrange(0, altura)
     direcao = True
     velocidade = 1
-    posicaoXKuduairo = 500
-    posicaoYKuduairo = 100
-    movimentoXKuduairo = 0
-    movimentoYKuduairo = 0
+    posicaoXGoku = 500
+    posicaoYGoku = 100
+    movimentoXGoku = 0
+    movimentoYGoku = 0
     pontos = 0
-    kuduairo = pygame.image.load("assets/kuduairo.png")
-    brasil = pygame.image.load("assets/brasil.png")
-    pygame.mixer.music.load("assets/trilha.mp3")
-    pygame.mixer.music.play(-1)
-    pygame.mixer.music.set_volume(1)
+    goku = pygame.image.load("assets/goku.png")
+    freeza = pygame.image.load("assets/freeza.png")
 
-    alturaKuduairo = 483
-    larguraKuduairo = 638
-    alturaBrasil = 400
-    larguraBrasil = 400
+    alturaGoku = 100
+    larguraGoku = 60
+    alturaFreeza = 100
+    larguraFreeza = 60
     dificuldade = 29
     jogando = True
     while True:
@@ -62,30 +58,33 @@ def jogo():
                 if event.key == pygame.K_RETURN:
                     jogo()
                 if event.key == pygame.K_LEFT:
-                    movimentoXKuduairo = - 10
+                    movimentoXGoku = - 10
                 elif event.key == pygame.K_RIGHT:
-                    movimentoXKuduairo = 10
-                elif event.key == pygame.UP:
-                    movimentoYKuduairo = - 10
+                    movimentoXGoku = 10
+                elif event.key == pygame.K_UP:
+                    movimentoYGoku = - 10
                 elif event.key == pygame.K_DOWN:
-                    movimentoYKuduairo = 10
+                    movimentoYGoku = 10
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    movimentoXKuduairo = 0
-                    movimentoYKuduairo = 0
+                    movimentoXGoku = 0
+                    movimentoYGoku = 0
         
         if jogando == True:
-            posicaoXKuduairo = posicaoXKuduairo + movimentoXKuduairo
-            posicaoYKuduairo = posicaoYKuduairo + movimentoYKuduairo
-            if posicaoXKuduairo < 0:
-                posicaoXKuduairo = 0
-            elif posicaoXKuduairo >= largura - larguraKuduairo:
-                posicaoXKuduairo = largura - larguraKuduairo
+            posicaoXGoku = posicaoXGoku + movimentoXGoku
+            posicaoYGoku = posicaoYGoku + movimentoYGoku
+            if posicaoXGoku < 0:
+                posicaoXGoku = 0
+            elif posicaoXGoku >= largura - larguraGoku:
+                posicaoXGoku = largura - larguraGoku
             
-            if posicaoYKuduairo < 0:
-                posicaoYKuduairo = 0
-            elif posicaoYKuduairo >= altura - alturaKuduairo:
-                posicaoYKuduairo = altura - alturaKuduairo
+            if posicaoYGoku < 0:
+                posicaoYGoku = 0
+            elif posicaoYGoku >= altura - alturaGoku:
+                posicaoYGoku = altura - alturaGoku
+
+            gameDisplay.blit(bg, (0, 0))
+                
             
             if direcao == True:
                 if posicaoX < largura-150:
@@ -103,20 +102,20 @@ def jogo():
                     posicaoY = random.randrange(0, altura)
                     velocidade = velocidade + 1
                     pontos = pontos + 1
-            gameDisplay.blit(brasil, (posicaoX, posicaoY))
-            gameDisplay.blit(kuduairo, (posicaoXKuduairo, posicaoYKuduairo))
+            gameDisplay.blit(freeza, (posicaoX, posicaoY))
+            gameDisplay.blit(goku, (posicaoXGoku, posicaoYGoku))
             fonte = pygame.font.Font("freesansbold.ttf", 20)
             texto = fonte.render("Poaintos: "+str(pontos), True, white)
             gameDisplay.blit(texto, (20, 20))
 
-            pixelsYKuduairo = list(range(posicaoYKuduairo, posicaoYKuduairo + alturaKuduairo + 1))
-            pixelsXKuduairo = list(range (posicaoXKuduairo, posicaoXKuduairo + larguraKuduairo + 1))
+            pixelsYGoku = list(range(posicaoYGoku, posicaoYGoku + alturaGoku + 1))
+            pixelsXGoku = list(range (posicaoXGoku, posicaoXGoku + larguraGoku + 1))
 
-            pixelsYBrasil = list(range(posicaoY, posicaoY + alturaBrasil + 1))
-            pixelsXBrasil = list(range(posicaoX, posicaoX + larguraBrasil + 1))
+            pixelYFreeza = list(range(posicaoY, posicaoY + alturaFreeza + 1))
+            pixelXFreeza = list(range(posicaoX, posicaoX + larguraFreeza + 1))
 
-            if len(list(set(pixelsYBrasil) & set(pixelsXKuduairo))) > dificuldade:
-                if len (list(set(pixelsXBrasil) & set(pixelsYBrasil))) > dificuldade:
+            if len(list(set(pixelYFreeza) & set(pixelsYGoku))) > dificuldade:
+                if len (list(set(pixelXFreeza) & set(pixelsXGoku))) > dificuldade:
                     jogando = False
                     dead(pontos)
         pygameDisplay.update()
